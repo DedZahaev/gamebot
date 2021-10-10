9b34121a9f355e7c7dd182bf248d798a1d42b490
Danila Marychev <marychtv5@gmail.com>
2021-10-10 07:33:29 +0300

    Усложненное задание

--- script.js
+++ script.js
@@ -9,24 +9,37 @@ const nullcheck = function (num) {
     }
 }
 
-function number(x) {
+function number(x, y) {
     function ask1() {
-        const ask = prompt("Угадай число от 1 до 100");
-        if (ask > x) {
-            alert("Загаданное число меньше");
+        let ask = prompt("Угадай число от 1 до 100");
+        y--;
+        if (ask > x && y != 0) {
+            alert("Загаданное число меньше, осталось попыток " + y);
             ask1();
-        } else if (ask < x && ask != null) {
-            alert("Загаданное число больше");
+        } else if (ask < x && ask != null && y != 0) {
+            alert("Загаданное число больше , осталось попыток " + y);
             ask1();
         } else if (isNumber(ask) == false && ask != null) {
             alert("Введи число!");
             ask1();
         } else if (ask === null) {
-            alert("Игра окончена");
+            alert("Завершается");
         } else if (ask == x) {
-            alert("Вы угадали!!!");
+            ask = prompt("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+            if (ask == "") {
+                y = 10;
+                ask1();
+            }
+        } else if (y == 0) {
+            ask = prompt("Попытки закончились, хотите сыграть еще?");
+            if (ask == "") {
+                y = 10;
+                ask1();
+            }
         }
+
+
     }
     ask1();
 }
-number(50);
+number(50, 10);
